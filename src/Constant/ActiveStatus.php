aa7a466f79a3df32b1a460389962f0a7cfee465a
冷斌 <xykxyk2008@163.com>
2021-03-09 18:51:29 +0800

    fix error code  path

---
+++ src/Constant/ActiveStatus.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Constants\Status\Platform;
+
+use EasySwoole\Skeleton\Framework\BaseEnum;
+
+class ActiveStatus extends BaseEnum
+{
+    /**
+     * @Message("启用")
+     */
+    const ENABLE = 1;
+
+    /**
+     * @Message("未启用")
+     */
+    const DISABLE = 1;
+}
