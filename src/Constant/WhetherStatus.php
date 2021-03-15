6f7f8696811c1a5f411a25af5cd92c826b88eab5
冷斌 <xykxyk2008@163.com>
2021-03-15 15:33:30 +0800

    add permission assign

---
+++ src/Constant/WhetherStatus.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace EasySwoole\Skeleton\Constant;
+
+use EasySwoole\Skeleton\Framework\BaseEnum;
+
+class WhetherStatus extends BaseEnum
+{
+    /**
+     * @Message("是")
+     */
+    const YES = 1;
+
+    /**
+     * @Message("否")
+     */
+    const NO = 2;
+}
