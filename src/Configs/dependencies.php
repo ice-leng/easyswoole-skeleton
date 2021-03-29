45f252e5de5bddf2f75b70ffacf58efe05da952f
冷斌 <xykxyk2008@163.com>
2021-03-29 18:33:58 +0800

    di

--- src/Configs/dependencies.php
+++ src/Configs/dependencies.php
@@ -1,12 +1,15 @@
 <?php
 
-use EasySwoole\Skeleton\Utility\CloudStorage\CloudStorage;
+use EasySwoole\Skeleton\Utility\Alipay\Alipay;
 use EasySwoole\Skeleton\Utility\CloudStorage\Impl\AliyunOSS;
 use EasySwoole\Skeleton\Utility\CloudStorage\Impl\QiNiuObject;
 use EasySwoole\Skeleton\Utility\Sms\Impl\AliyunSms;
+use EasySwoole\WeChat\WeChat;
 
 return [
     AliyunOSS::class   => AliyunOSS::class,
     QiNiuObject::class => QiNiuObject::class,
     AliyunSms::class   => AliyunSms::class,
+    WeChat::class      => WeChat::class,
+    Alipay::class      => Alipay::class,
 ];
