c99c13fa4c998d52578e72250f882deb31ec9991
冷斌 <xykxyk2008@163.com>
2021-04-02 23:02:00 +0800

    fix bug

--- src/Utility/Sms/RedisSms.php
+++ src/Utility/Sms/RedisSms.php
@@ -9,18 +9,18 @@ class RedisSms
 
     const SMS_CODE_PREFIX = 'sms_code_%s_%s';
 
-    public function key(string $mobile, string $template): string
+    protected static function key(string $mobile, string $template): string
     {
         return sprintf(self::SMS_CODE_PREFIX, $template, $mobile);
     }
 
-    public function get(string $mobile, string $template)
+    public static function get(string $mobile, string $template)
     {
-        return RedisPool::defer('redis')->get($this->key($mobile, $template));
+        return RedisPool::defer('redis')->get(self::key($mobile, $template));
     }
 
-    public function set(string $mobile, string $template, $value, int $expire = null)
+    public static function set(string $mobile, string $template, $value, int $expire = null)
     {
-        RedisPool::defer('redis')->set($this->key($mobile, $template), $value, $expire);
+        RedisPool::defer('redis')->set(self::key($mobile, $template), $value, $expire);
     }
 }
