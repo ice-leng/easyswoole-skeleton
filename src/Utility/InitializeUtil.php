81388132cfe7babca3fdd0b1cc4c3d0b286bc1d9
冷斌 <xykxyk2008@163.com>
2021-04-09 14:55:38 +0800

    fix bug

--- src/Utility/InitializeUtil.php
+++ src/Utility/InitializeUtil.php
@@ -45,7 +45,7 @@ class InitializeUtil
             $response->withHeader('Access-Control-Allow-Origin', '*');
             $response->withHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
             $response->withHeader('Access-Control-Allow-Credentials', 'true');
-            $response->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+            $response->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Token');
             if ($request->getMethod() === 'OPTIONS') {
                 $response->withStatus(Status::CODE_OK);
                 return false;
