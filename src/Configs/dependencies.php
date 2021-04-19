1c714e4d758cfddecfa206db0261fc8ea1e9edba
冷斌 <xykxyk2008@163.com>
2021-04-19 17:52:10 +0800

    add cache

--- src/Configs/dependencies.php
+++ src/Configs/dependencies.php
@@ -5,6 +5,8 @@ use EasySwoole\Skeleton\Utility\CloudStorage\Impl\AliyunOSS;
 use EasySwoole\Skeleton\Utility\CloudStorage\Impl\QiNiuObject;
 use EasySwoole\Skeleton\Utility\Sms\Impl\AliyunSms;
 use EasySwoole\Skeleton\Utility\WeChat\WeChat;
+use EasySwoole\Skeleton\Component\Cache\SimpleCache;
+use Psr\SimpleCache\CacheInterface;
 
 return [
     AliyunOSS::class   => AliyunOSS::class,
@@ -12,4 +14,5 @@ return [
     AliyunSms::class   => AliyunSms::class,
     WeChat::class      => WeChat::class,
     Alipay::class      => Alipay::class,
+    CacheInterface::class => SimpleCache::class,
 ];
