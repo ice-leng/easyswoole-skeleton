e5a2cbfd1ea36c4c871ce8c1a63e5637b8092030
冷斌 <xykxyk2008@163.com>
2021-04-19 15:41:21 +0800

    add cache

--- src/Component/Cache/SimpleCache.php
+++ src/Component/Cache/SimpleCache.php
@@ -9,15 +9,65 @@
 
 namespace EasySwoole\Skeleton\Component\Cache;
 
-class SimpleCache
+use Psr\SimpleCache\CacheInterface;
+
+class SimpleCache implements CacheInterface
 {
-    /**
-     * @var SimpleCacheConfig
-     */
-    protected $config;
+
+    protected $driver;
 
     public function __construct(SimpleCacheConfig $config)
     {
-        $this->config = $config;
+        $driver = $config->getDriver();
+        $this->driver = new $driver([
+            'dir'    => $config->getDir(),
+            'prefix' => $config->getPrefix(),
+            'packer' => $config->getPacker(),
+        ]);
+    }
+
+    public function __call($name, $arguments)
+    {
+        return $this->driver->{$name}(...$arguments);
+    }
+
+    public function get($key, $default = null)
+    {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function set($key, $value, $ttl = null)
+    {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function delete($key)
+    {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function clear()
+    {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function getMultiple($keys, $default = null)
+    {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function setMultiple($values, $ttl = null)
+    {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function deleteMultiple($keys)
+    {
+        return $this->__call(__FUNCTION__, func_get_args());
+    }
+
+    public function has($key)
+    {
+        return $this->__call(__FUNCTION__, func_get_args());
     }
 }
