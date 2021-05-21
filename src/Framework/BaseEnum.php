2b0daa70ad3e12175e1dc30a1385daf37f8d8ebe
冷斌 <xykxyk2008@163.com>
2021-05-22 00:06:44 +0800

    fix bug

--- src/Framework/BaseEnum.php
+++ src/Framework/BaseEnum.php
@@ -9,7 +9,10 @@ class BaseEnum extends AbstractEnum
 {
     public function getMessage(array $replace = []): string
     {
-        // todo  多语言 翻译
-        return parent::getMessage($replace);
+        $message = parent::getMessage($replace);
+        if (function_exists('__')) {
+            $message = __($message, $replace);
+        }
+        return $message;
     }
 }
