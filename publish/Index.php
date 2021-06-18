522b503c94226541dd5c7aa328ce8b2f11c988ad
冷斌 <xykxyk2008@163.com>
2021-06-18 13:17:21 +0800

    fix bug

--- publish/Index.php
+++ publish/Index.php
@@ -2,12 +2,32 @@
 
 namespace App\HttpController;
 
-use EasySwoole\Skeleton\Framework\BaseController;;
+use EasySwoole\Http\AbstractInterface\Controller;
 
-class Index extends BaseController
+class Index extends Controller
 {
+
     public function index()
     {
-        $this->response()->redirect("./index.html");
+        $file = EASYSWOOLE_ROOT.'/vendor/easyswoole/easyswoole/src/Resource/Http/welcome.html';
+        if(!is_file($file)){
+            $file = EASYSWOOLE_ROOT.'/src/Resource/Http/welcome.html';
+        }
+        $this->response()->write(file_get_contents($file));
+    }
+
+    function test()
+    {
+        $this->response()->write('this is test');
+    }
+
+    protected function actionNotFound(?string $action)
+    {
+        $this->response()->withStatus(404);
+        $file = EASYSWOOLE_ROOT.'/vendor/easyswoole/easyswoole/src/Resource/Http/404.html';
+        if(!is_file($file)){
+            $file = EASYSWOOLE_ROOT.'/src/Resource/Http/404.html';
+        }
+        $this->response()->write(file_get_contents($file));
     }
 }
--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -83,12 +83,6 @@ class ConfigProvider
                     'destination' => EASYSWOOLE_ROOT . '/App/HttpController/Router.php',
                 ],
                 [
-                    'id' => 'swagger',
-                    'description' => 'The config for controller swagger.',
-                    'source' => dirname(__DIR__) . '/publish/Swagger.php',
-                    'destination' => EASYSWOOLE_ROOT . '/App/HttpController/Swagger.php',
-                ],
-                [
                     'id' => 'dev',
                     'description' => 'The config dev',
                     'source' => dirname(__DIR__) . '/publish/dev.php',
